8a49eb0c9864f2a50e17fd555f7ca390bb5a2bd2
Jennifer Bordon <jennifer.bordon@gmail.com>
2021-06-01 14:06:31 -0700

    add webpage with squid and chicken
    Co-authored-by: Garrett Broughton <gbrough@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,12 @@
 $(document).ready(function() {
-  $("h1").click(function() {
-    alert("This is a heading.");
+  $(".expand").click(function() {
+    $('.sec1-hidden').toggle();
+    $('.sec1-showing').toggle();
+    $('.expanded').show();
   });
-
-  $("p").click(function() {
-    alert("This is a paragraph.");
-  });
-
-  $("img").click(function() {
-    alert("This is an image.");
+  $(".expand2").click(function() {
+    $('.sec2-hidden').toggle();
+    $('.sec2-showing').toggle();
+    $('.expanded2').show();
   });
 });
